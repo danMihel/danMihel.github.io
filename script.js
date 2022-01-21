1b5cb65881fcda7f09368e818016be51cb471958
danMihel <d.mihel78@gmail.com>
2022-01-21 18:44:29 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 window.addEventListener('DOMContentLoaded', function() {
     const btn = document.querySelector('.startBtn'),
           display = document.querySelectorAll('.display'),
-          title = document.querySelector('.titleMove');
-    
-          let i = 100;
+          title = document.querySelector('.titleMove'),
+          sceneTwo = document.querySelector('.sceneTwo'),
+          closeBtn = document.querySelector('.closeBtn');
+         
+
+          closeBtn.addEventListener('click', ()=>{ 
+            sceneTwo.style.display = "none";
+            display.forEach(item => {
+                item.classList.toggle('hide');                 
+                setTimeout(() => item.classList.toggle('nomarg'), 100);  
+                setTimeout(() => item.style.display = "block", 1100);        
+            });  
+          });
 
 
         btn.addEventListener('click', ()=>{ 
-            setInterval(() =>   title.style.height =  `${i--}px `, 11);       
+            let i = 100;
+            setInterval(() => title.style.height =  `${i--}px `, 11);                 
+            setTimeout(() => sceneTwo.classList.add('b-show'), 900);  
+            
+
             display.forEach(item => {
-                item.classList.add('hide');  
-               
+                item.classList.add('hide');                 
                 setTimeout(() => item.classList.add('nomarg'), 100);  
-               /*  setTimeout(() => item.style.display = "none", 1700);   */
-            
-            });
-            
-        
+                setTimeout(() => item.style.display = "none", 1100);        
+            });   
+          
     });
    
 });
-
 
